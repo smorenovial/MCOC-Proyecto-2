c2e9d8eb8638f02ed76d407cecc97e1a7c88dab9
smorenovial <53713496+smorenovial@users.noreply.github.com>
2019-10-11 21:03:33 -0300

    Update Saltation_many_particles_validation.py

--- Saltation_many_particles_validation.py
+++ Saltation_many_particles_validation.py
@@ -13,7 +13,10 @@ _gr = 1e-3*_kg
 _in = 2.54*_cm
 
 g = 9.81*_m/(_s**2)         # gravedad
-d = 15.*_mm
+d = 0.15e-3
+x = linspace(0, 20*d,4000)
+x_mod_d = (x % d) - d/2
+y = sqrt((d/2)**2 - x_mod_d**2)
 
 rho_agua = 1000.*_kg/(_m**3)                   # diametro de la particula
 rho_particula = 2650*_kg/(_m**3)      # densidad de la particula, considerando que sea arena 
@@ -22,14 +25,12 @@ dt = 0.001*_s    # paso de tiempo
 tmax = 0.5*_s    # tiempo maximo de simulacion
 ti = 0.*_s     # tiempo actual
 
-nparticulas = 5
-
-
-x0 = 10*d*rand(nparticulas)
-y0 = 3*d*rand(nparticulas) + d
-
-vx0 = rand(nparticulas)/2
-vy0 = rand(nparticulas)/2
+data = load("initial_condition.npz")
+x0 = data["x0"]
+y0 = data["y0"]
+vx0 = data["vx0"]
+vy0 = data["vy0"]
+Nparticulas = data["Nparticulas"]
 
 
 A = pi*(d/2)**2
@@ -55,7 +56,16 @@ ihat = array ([1,0])
 jhat = array ([0,1])
 
 ustar = 0.14 # en el paper sale que es entre 0.14 y 0.23 
-def velocitiy_field(x):
+tau_star = 0.067
+tau_cr = 0.22*Rp**(-0.6)+0.06*10**(-7*Rp**(-0.6))
+ustar = sqrt(tau_star*g*Rp*d)
+
+print "tau_star = ", tau_star
+print "tau_cr 0 ", tau_cr
+print "tau_star/tau_cr = ", tau_star/tau_cr
+print "ustar = ", ustar
+
+def velocity_field(x):
 	z = x[1] / d
 	if z > 1./30:
 		uf =ustar*log(30.*z)/0.41
@@ -64,49 +74,8 @@ def velocitiy_field(x):
 	return array([uf,0])
 
 
-vfx = velocitiy_field([0,4*d])[0]
-k_penal = 100.*0.5*Cd*rho_agua*A*norm(vfx)**2/(1*_mm) 
-
-
-def particula(z,t):
-	zp = zeros(4*nparticulas)
-
-	for i in range(nparticulas):
-		di = d
-		xi = z[4*i:(4*i+2)]
-		vi = z[4*i+2:(4*i+4)]
-
-		vf = velocitiy_field(xi)
-		vf_top = norm(velocitiy_field(xi + (di/2)*jhat))
-		vf_bot = norm(velocitiy_field(xi - (di/2)*jhat))
-		vrel = vf - vi
-		fD = (0.5*Cd*alpha*rho_agua*norm(vrel)*A)*vrel 
-		fL = (0.5*CL*alpha*rho_agua*(vf_top**2 -vf_bot**2)*A)*jhat # en esta linea falta codigo
-		
-		Fi = W + fD + fL
-
-		if xi[1] < 0:
-			Fi += -k_penal*xi[1]
-
-		zp[4*i:(4*i+2)] = vi
-		zp[4*i+2:(4*i+4)] = Fi/m
-
-	for i in range(nparticulas):
-		xi = z[4*i:(4*i+2)]
-		for j in  range(nparticulas):
-			if i > j:
-				xj = z[4*j:(4*j+2)]
-				rij = xj -xi
-				if norm(rij) < d:
-					delta = d - norm(rij)
-					nij = rij/norm(rij)
-					Fj = k_penal*delta*nij
-					Fi = -k_penal*delta*nij
-					zp[4*i+2:(4*i+4)] += Fi/m
-					zp[4*j+2:(4*j+4)] += Fj/m
-	
-	return zp
-
+vfx = velocity_field([0,4*d])[0]
+k_penal = 100.*0.5*Cd*rho_agua*A*norm(vfx)**2/(1*_mm)
 
 def fuerzas_hidrodinamicas(x,v,d,area,masa):
 
@@ -130,7 +99,47 @@ def fuerzas_hidrodinamicas(x,v,d,area,masa):
 	return Fh
 
 
-z0 = zeros(4*nparticulas)
+def particula(z,t):
+	zp = zeros(4*Nparticulas)
+
+	for i in range(Nparticulas):
+		di = d
+		xi = z[4*i:(4*i+2)]
+		vi = z[4*i+2:(4*i+4)]
+
+
+		Fi = fuerzas_hidrodinamicas(xi,vi,di,A,m)
+		x_mod_d = (xi[0] % d) - d/2
+		y = sqrt((d/2)**2 - x_mod_d**2)
+
+		
+		if xi[1] < 0:
+			Fi[1] += -k_penal*xi[1]
+
+		elif xi[1] <= y:
+			Fi[1] += k_penal*xi[1]
+
+		zp[4*i:(4*i+2)] = vi
+		zp[4*i+2:(4*i+4)] = Fi/m
+
+		for i in range(Nparticulas):
+			xi = z[4*i:(4*i+2)]
+			for j in  range(Nparticulas):
+				if i > j:
+					xj = z[4*j:(4*j+2)]
+					rij = xj -xi
+					if norm(rij) < d:
+						delta = d - norm(rij)
+						nij = rij/norm(rij)
+						Fj = k_penal*delta*nij
+						Fi = -k_penal*delta*nij
+						zp[4*i+2:(4*i+4)] += Fi/m
+						zp[4*j+2:(4*j+4)] += Fj/m
+	
+	return zp
+
+
+z0 = zeros(4*Nparticulas)
 z0[0::4] = x0
 z0[1::4] = y0
 z0[2::4] = vx0
@@ -145,7 +154,7 @@ fig = figure()
 
 ax = gca() # esto es para la linea de suelo
 
-for i in range(nparticulas):
+for i in range(Nparticulas):
 	xi = z[:,4*i]
 	yi = z[:,4*i+1]
 	col = rand(4)
@@ -155,17 +164,13 @@ for i in range(nparticulas):
 ax.axhline(d/2,color = "k", linestyle = "--")
 
 
-d = 0.15e-3
-x = linspace(0, 20*d,4000)
-x_mod_d = (x % d) - d/2
-y = sqrt((d/2)**2 - x_mod_d**2)
 
-plot(x, y)
-axis("equal")
+#plot(x, y)
 
 tiempoFinal = time()
 
 tiempoTotal = tiempoFinal - tiempoInicial
 print tiempoTotal
+"numero particulas = ", Nparticulas
+#axis("equal")
 show()
-
